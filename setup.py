21185b35a6d971ebf8192439a6cf7c5f279d6af3
YuvalShmaryahu <100292417+YuvalShmaryahu@users.noreply.github.com>
2023-09-01 15:59:04 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import Extension, setup
-module=Extension("symnmfsp", sources=["symnmfmodule.c", "symnmf.c"])
-setup(name="symnmfsp",
+module=Extension("mysymnmfsp", sources=["symnmfmodule.c", "symnmf.c"])
+setup(name="mysymnmfsp",
       version="1.0",
       description="Python wrapper for custom C extension",
       ext_modules=[module]
